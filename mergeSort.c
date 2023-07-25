795d25f3c576b64eaa2706c9cfff8bbbd15782f3
M ANANTA NAGA RAJESH <98514984+anantanagarajesh@users.noreply.github.com>
2023-07-25 23:20:56 +0530

    Add files via upload

---
+++ mergeSort.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<time.h>
+void merge(int A[], int si, int mid, int ei)
+{
+    int i, j, k, c[100];
+    i = si;
+    j = mid + 1;
+    k = si;
+
+    while (i <= mid && j <= ei)
+    {
+        if (A[i] < A[j])
+            c[k++] = A[i++];
+        else
+            c[k++] = A[j++];
+    }
+    while (i <= mid)
+        c[k++] = A[i++];
+    while (j <= ei)
+        c[k++] = A[j++];
+
+    for (i = si; i <= ei; i++)
+    {
+        A[i] = c[i];
+    }
+}
+
+void mergeSort(int A[], int si, int ei)
+{
+    int mid;
+    if (si < ei)
+    {
+        mid = (si + ei) / 2;
+        mergeSort(A, si, mid);
+        mergeSort(A, mid + 1, ei);
+        merge(A, si, mid, ei);
+    }
+}
+
+int main()
+{
+    int n;
+    int A[10];
+    printf("\nEnter the number of elements: ");
+    scanf("%d", &n);
+    printf("Enter array elements\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &A[i]);
+    }
+    
+    clock_t st,et;
+    double ts;
+    st=clock();
+    printf("Array Elements: \n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", A[i]);
+    }
+
+    printf("\nSorted Array: \n");
+    mergeSort(A, 0, n - 1);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", A[i]);
+    }
+    et=clock();
+    ts=(double)(et-st)/CLOCKS_PER_SEC;
+    printf("\nTime taken: %f",ts);
+}
