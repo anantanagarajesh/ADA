6ed14330191aa0976de72381d15c7bdfe15f413f
M ANANTA NAGA RAJESH <98514984+anantanagarajesh@users.noreply.github.com>
2023-08-05 12:15:13 +0530

    Update Floyds_algorithm.c

--- Floyds_algorithm.c
+++ Floyds_algorithm.c
@@ -2,9 +2,23 @@
 #define V 4
 #define INF 99999
 
-void printSolution(int dist[][V]);
+void printSolution(int dist[][V])
+{
+	printf(
+		"The following matrix shows the shortest distances"
+		" between every pair of vertices \n");
+	for (int i = 0; i < V; i++) {
+		for (int j = 0; j < V; j++) {
+			if (dist[i][j] == INF)
+				printf("%7s", "INF");
+			else
+				printf("%7d", dist[i][j]);
+		}
+		printf("\n");
+	}
+}
 
-void floydWarshall(int dist[][V])
+void floyd(int dist[][V])
 {
 	int i, j, k;
 
@@ -22,38 +36,13 @@ void floydWarshall(int dist[][V])
 	printSolution(dist);
 }
 
-void printSolution(int dist[][V])
-{
-	printf(
-		"The following matrix shows the shortest distances"
-		" between every pair of vertices \n");
-	for (int i = 0; i < V; i++) {
-		for (int j = 0; j < V; j++) {
-			if (dist[i][j] == INF)
-				printf("%7s", "INF");
-			else
-				printf("%7d", dist[i][j]);
-		}
-		printf("\n");
-	}
-}
-
 int main()
 {
-	/* Let us create the following weighted graph
-			10
-	(0)------->(3)
-		|		 /|\
-	5 |		 |
-		|		 | 1
-	\|/		 |
-	(1)------->(2)
-			3		 */
 	int graph[V][V] = { { 0, 5, INF, 10 },
 						{ INF, 0, 3, INF },
 						{ INF, INF, 0, 1 },
 						{ INF, INF, INF, 0 } };
 
-	floydWarshall(graph);
+	floyd(graph);
 	return 0;
 }
