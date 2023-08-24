d2d52a5abe57b90b8150bf81bf9f57f7c5956449
M ANANTA NAGA RAJESH <98514984+anantanagarajesh@users.noreply.github.com>
2023-08-24 20:44:41 +0530

    Update Knapsack.c

--- Knapsack.c
+++ Knapsack.c
@@ -1,36 +1,26 @@
-// A Dynamic Programming based
 // solution for 0-1 Knapsack problem
 #include <stdio.h>
-
-// A utility function that returns
-// maximum of two integers
+// A utility function that returns maximum of two integers
 int max(int a, int b) { return (a > b) ? a : b; }
 
-// Returns the maximum value that
-// can be put in a knapsack of capacity W
+// Returns the maximum value that can be put in a knapsack of capacity W
 int knapSack(int W, int wt[], int val[], int n)
 {
-	int i, w;
-	int K[n + 1][W + 1];
-
-	// Build table K[][] in bottom up manner
-	for (i = 0; i <= n; i++) {
-		for (w = 0; w <= W; w++) {
-			if (i == 0 || w == 0)
-				K[i][w] = 0;
-			else if (wt[i - 1] <= w)
-				K[i][w] = max(val[i - 1]
-								+ K[i - 1][w - wt[i - 1]],
-							K[i - 1][w]);
+	int v[n + 1][W + 1];
+	// Build table v[][] in bottom up manner
+	for (int i = 0; i <= n; i++) {
+		for (int j= 0; j <= W; j++) {
+			if (i == 0 || j == 0)
+				v[i][j] = 0;
+			else if (wt[i - 1] <= j)
+				v[i][j] = max(val[i - 1]+ v[i - 1][j- wt[i - 1]],v[i - 1][j]);
 			else
-				K[i][w] = K[i - 1][w];
+				v[i][j] = v[i - 1][j];
 		}
 	}
 
-	return K[n][W];
+	return v[n][W];
 }
-
-// Driver Code
 int main()
 {
 	int profit[] = { 60, 100, 120 };
